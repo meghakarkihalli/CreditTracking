6a0aef7c924fee96b6ce21ccf0dcda4fd84f7dd7
meghakarkihalli <meghakarkihalli@gmail.com>
2020-08-15 10:30:39 -0700

    initial commit

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Welcome to Credit Tracking
+      </div>
+    )
+  }
+}
+export default App;
