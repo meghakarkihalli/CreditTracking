13c307a5b961050523112c694c0d93372eb172fe
tcho <tcho@cca.edu>
2020-08-15 22:21:34 -0700

    imports the Logo into App.js and renders it the return

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Logo from './component-library/components/icon/index'
 
 class App extends Component {
   render() {
     return (
       <div>
         Welcome to Credit Tracking
+        <Logo />
       </div>
     )
   }
