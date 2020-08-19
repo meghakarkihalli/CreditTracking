28d780b143a234c57ec17f35d17906827c22398e
jasmine <khehragurmeet@gmail.com>
2020-08-19 12:32:48 -0700

    Accordion

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Logo from './component-library/components/icon/index'
+import ControlledAccordions from './component-library/components/accordian';
+
 
 class App extends Component {
   render() {
     return (
       <div>
+          
         <Logo />
-        Welcome to Credit Tracking
+        <ControlledAccordions />
+     
       </div>
     )
   }
---
+++ app/src/component-library/components/accordian/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%"
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: "33.33%",
+    flexShrink: 0
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary
+  }
+}));
+
+function ControlledAccordions() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const data = [
+    {
+      id: "panel1",
+      heading: "Panel 1",
+      // secondaryHeading: "this is panel 1",
+      details: "My first experience with accordian"
+    },
+    {
+      id: "panel2",
+      heading: "Panel 2",
+      // secondaryHeading: "this is panel 2",
+      details: " Reusable accordian components "
+    }
+  ];
+
+  return (
+    <div className={classes.root}>
+      {data.map(accordion => {
+        const { id, heading, secondaryHeading, details } = accordion;
+        return (
+          <Accordion
+            expanded={expanded === id}
+            key={id}
+            onChange={handleChange(id)}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography className={classes.heading}>{heading}</Typography>
+              <Typography className={classes.secondaryHeading}>
+                {secondaryHeading}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>{details}</Typography>
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
+    </div>
+  );
+}
+export default ControlledAccordions;
