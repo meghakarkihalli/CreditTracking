a568144e84fbdeaa00f3e0a975137db6784de8f8
Sandip Prajapati <sandipprajapati@Sandips-MacBook-Pro.local>
2020-08-20 11:57:23 -0700

    updated changes

--- app/src/component-library/components/button/index.js
+++ app/src/component-library/components/button/index.js
@@ -1,34 +1,11 @@
-import React, {Component} from 'react';
-import style from  './style.scss';
-// import { Button } from '@material-ui/core';
+import React, { Component } from 'react';
+import style from './style.scss';
 
- 
-class Button extends Component{
-    constructor(props){
-        super(props)
-        this.state={
-            default_class:this.props.btn_type,
-            text:"Secondary"
-        }
-
-    }
-    
-    onHandleClick = (text) =>{
-        this.setState({default_class:"Primary_Class"})
-        this.setState({text});
-        
-       
-    }
-    
-    render(){
-        const {text} =this.state;
-
-        return(
-            <button onClick ={ () =>{this.onHandleClick("Primary")}} className={this.state.default_class}>{this.state.text}</button>
-        );
-    }
+function Button(props) {
+    return (
+        <button onClick={props.my_function} className={props.btn_class}>{props.children}</button>
+    );
 }
-
 export default Button;
 
 
