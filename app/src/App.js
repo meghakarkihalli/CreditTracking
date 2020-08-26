2927adb235ab3f04adbc9ca2eca2adf5014bf4f3
jasmine <khehragurmeet@gmail.com>
2020-08-26 14:10:06 -0700

    update alpha

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Logo from './component-library/components/icon/index'
-import ControlledAccordions from './component-library/components/accordian';
+import Accordion from './component-library/components/accordian';
+
 
 
 class App extends Component {
@@ -10,7 +11,23 @@ class App extends Component {
       <div>
           
         <Logo />
-        <ControlledAccordions />
+        <div>
+        <Accordion>
+          <div className="accor">
+            <div className="head">Head 1</div>
+            <div className="body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+          </div>
+          <div className="accor">
+            <div className="head">Head 2</div>
+            <div className="body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+          </div>
+        </Accordion>
+      </div>
+       
      
       </div>
     )
