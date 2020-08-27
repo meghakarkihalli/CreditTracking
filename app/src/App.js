2fc3c4fabd68c9b8f1adfa656cd225d229a2d8b0
tejalsandip <60160214+tejalsandip@users.noreply.github.com>
2020-08-26 17:05:28 -0700

    Revert "Buttontask"

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Logo from './component-library/components/icon/index'
-import Button from './component-library/components/button';
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <Button btn_type="Secondary_Class"/>
+
         <Logo />
         Welcome to Credit Tracking
 
