fb5c6a011c162b7b69a0bb1862d973c2fa0cc800
Sandip Prajapati <sandipprajapati@Sandips-MacBook-Pro.local>
2020-08-28 14:50:11 -0700

    ButtonTask Updated

--- app/src/component-library/components/button/index.js
+++ app/src/component-library/components/button/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import style from './style.scss';
+// import { modes } from 'react-transition-group/SwitchTransition';
 
-function Button(props) {
+const Button = (props) => {
+    const {clickHandler, btn_class} = props;
     return (
-        <button onClick={props.my_function} className={props.btn_class}>{props.children}</button>
+        <button onClick={clickHandler}  className={btn_class}>{props.children}</button>
     );
 }
 export default Button;
-
-
 
