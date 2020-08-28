2bec657a1be86d1e0e0820702e3c3d5319257d33
Priyanka Patel <61297173+Priyankaa-patel@users.noreply.github.com>
2020-08-28 15:49:36 -0700

    tooltip initial commit

---
+++ app/src/component-library/components/tooltip/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './style.scss';
+import Button from '../button';
+
+const Tooltip = (props) => {
+
+    const [active, setActive] = useState(false);
+    let timeout;
+    const { navigationList = [], children, rightSideImage, content, leftAddonLink } = props;
+
+
+    const showTip = () => {
+        clearInterval(timeout);
+        setActive(true);
+    }
+
+    const hideTip = () => {
+        timeout = setTimeout(() => {
+            setActive(false);
+        }, 400);
+    };
+
+    return (
+        <div className='Tooltip-Wrapper' style={{ left: '30%' }}
+            // When to show the tooltip
+            onMouseEnter={showTip}
+            onMouseLeave={hideTip}>
+
+            {/* Wrapping */}
+            {children}
+            {active && (
+                <div className='Tooltip-Tip'>
+                    {/* Content */}
+                    <div className='leftContent'>
+                        <ul>
+                            {navigationList.map((list, index) => (
+                                <li key={index}>{list}</li>
+                            ))}
+                        </ul>
+                        <div className='left-addon'>{leftAddonLink}</div>
+                    </div>
+                    <div className='rightContent'>
+                        <img src={rightSideImage} alt='spendy logo' />
+                        <p className='rightSideText'>{content}</p>
+                        <Button bordered>Find out how</Button>
+                    </div>
+                </div>)
+            }
+        </div>
+    );
+}
+
+export default Tooltip;
